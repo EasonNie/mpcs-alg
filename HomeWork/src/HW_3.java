8a8aade6e56e165f217ab882ad0340cfe3ea5a25
Eason Nie <Eason@appledeMacBook-Pro.local>
2015-10-18 22:20:31 -0500

    HW-3 Finished.

--- HomeWork/src/HW_3.java
+++ HomeWork/src/HW_3.java
@@ -1,16 +1,41 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by Eason on 10/16/15.
  */
 public class HW_3 {
     public static void main(String[] args) {
-        int[] test = new int[]{1, 4, 5, 8, 11};
-        int[] test2 = new int[]{16, 17, 23, 24, 39, 40};
-        int[] state = minCombo(test2, 100);
-        int[] result = reconstruct(test2, state);
-        System.out.println(Arrays.toString(result));
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            int k = Integer.parseInt(scanner.nextLine().trim());
+
+            if (line.equals("exit")) {
+                break;
+            }
+            String[] s_array = line.split(" ");
+            int[] array = new int[s_array.length];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Integer.parseInt(s_array[i]);
+            }
+
+            int[] state = minCombo(array, k);
+            int[] result = reconstruct(array, state);
+            for (int i = 0; i < result.length; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+        }
+
+//        int[] test = new int[]{1, 4, 5, 8, 11};
+//        int[] test2 = new int[]{16, 17, 23, 24, 39, 40};
+//        int[] state = minCombo(test2, 100);
+//        int[] result = reconstruct(test2, state);
+//        System.out.println(Arrays.toString(result));
         //System.out.println(minCombo(test2, 100)[100]);
+
     }
 
     public static int[] minCombo(int[] D, int n) {
